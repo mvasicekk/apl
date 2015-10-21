306d47fdd2dfb49ff7f2987a2fa8dce6bd02ac76
jr <jr@jrpc1>
2015-10-21 16:35:44 +0200

    rundlauf,ima ema

--- auftragpositionen/js/controllers.js
+++ auftragpositionen/js/controllers.js
@@ -195,14 +195,14 @@ aplApp.controller('detailController', function (setfocus,$filter,$scope, $routeP
 		    )
 		    .then(function (response) {
 			//presunout se na spravu zakazky
-			//$window.location.href = '../auftrag/auftrag.php#/det/'+response.data.auftrag;
-			$window.location.href = '../dauftr/dauftr.php?auftragsnr='+response.data.auftrag;
+			$window.location.href = '../auftrag/auftrag.php#/det/'+response.data.auftrag;
+//			$window.location.href = '../dauftr/dauftr.php?auftragsnr='+response.data.auftrag;
 		    });
     };
     
     $scope.endeClick = function(){
-	//$window.location.href = '../auftrag/auftrag.php#/det/'+$scope.auftragsnr;
-	$window.location.href = '../dauftr/dauftr.php?auftragsnr='+response.data.auftrag;
+	$window.location.href = '../auftrag/auftrag.php#/det/'+$scope.auftragsnr;
+//	$window.location.href = '../dauftr/dauftr.php?auftragsnr='+response.data.auftrag;
     };
     
     $scope.updateKzGut = function(r){
