f34395ad8764e1567cc140a3d8fe12baf0ad9aa1
jr <jr@jrpc1>
2016-05-19 13:30:59 +0200

    gauges, plneni planu

--- utils/gauges/js/controllers.js
+++ utils/gauges/js/controllers.js
@@ -6,165 +6,21 @@
 
 var aplApp = angular.module('gaugesApp');
 
-aplApp.directive('aplHelp', function () {
-	    
-	    return {
-		scope:{
-		    el:'=',
-		    showhelp:'=',
-		    showadmininfo:'='
-		},
-		restrict: 'E',
-		templateUrl: './templates/aplhelp.html',
-		link: function (scope, element, attrs) {
-		}
-	    }
-	});
-
-aplApp.directive("enterfocus", function () {
-        return {
-            restrict: "A",
-            link: function ($scope, elem, attrs) {
-                var focusables = $(":focusable");
-                elem.bind("keydown", function (e) {
-                    var code = e.keyCode || e.which;
-                    if (code === 13) {
-                        var current = focusables.index(this);
-                        var next = focusables.eq(current + 1).length ? focusables.eq(current + 1) : focusables.eq(0);
-			//console.log('current='+current+' next=');
-			//console.log(next);
-                        next.focus();
-			next.select();
-                        e.preventDefault();
-                    }
-                });
-            }
-        }
-});
-
-aplApp.controller('gaugesController', function ($scope, $http,$timeout,$window,$location) {
-
-    var statArrayOptions = [
-	    {
-		id:"lS0011",
-		unitString:'VzKd %',
-		titleString:'S0011',
-		threshold: 50
-	    },
-	    {
-		id:"lS0041",
-		unitString:'VzKd %',
-		titleString:'S0041',
-		threshold: 50
-	    },
-	    {
-		id:"lS0051",
-		unitString:'VzKd %',
-		titleString:'S0051',
-		threshold: 50
-	    },
-	    {
-		id:"lS0061",
-		unitString:'VzKd %',
-		titleString:'S0061',
-		threshold: 50
-	    }
-	    
-    ];
-    var gaugesArray = [];
-    
-    var linearS00XX = document.getElementById('linearS00XX');
-    
-    statArrayOptions.forEach(function(v){
-	var linear1 = new steelseries.Linear(v.id, {
-			    width: linearS00XX.offsetWidth,
-                            height: 140,
-                            titleString: v.titleString,
-                            unitString: v.unitString,
-                            threshold: v.threshold,
-                            lcdVisible: true
-                            });
-	linear1.setFrameDesign(steelseries.FrameDesign.METAL);			    
-	linear1.setBackgroundColor(steelseries.BackgroundColor.WHITE);
-	linear1.setValueColor(steelseries.ColorDef.RED);
-	linear1.setLcdColor(steelseries.LcdColor.BEIGE);
-	linear1.setLedVisible(false);
-	gaugesArray.push(linear1);
-    });
-    
-    //LCD hodiny + datum
-    var lcdHodiny = new steelseries.DisplaySingle('lcdHodiny', {
-                            width: document.getElementById('lcdHodinyContainer').offsetWidth,
-                            height: 100,
-			    valuesNumeric: false
-                            });
-    lcdHodiny.setLcdColor(steelseries.LcdColor.BEIGE);			    
-    setInterval(function(){ 
-	var today = Date();
-	lcdHodiny.setValue(Date().toLocaleString());
-    }, 500);			    
-    
-});
 
 /**
  * 
  */
 aplApp.controller('vzkdController', function ($scope, $http,$timeout,$window,$location,$interval) {
 
+    // promenne ----------------------------------------------------------------
     $scope.dt = new Date();
-    
-    $scope.getStatArray = function(){
-	return $http.post(
-		'../getStatNrPlan.php',
-		{kdvon:111,kdbis:195}
-		).then(function (response) {
-		    $scope.statNrArray = response.data.statArray;
-		    console.log('getStatArray');
-		    console.log($scope.statNrArray);
-		    var vzkdPlan = 0;
-		    if($scope.statNrArray!==null){
-			$scope.statNrArray.forEach(function(v){
-			    if(v.statnr!='S0091'){
-				$scope.statArray.forEach(function(v1){
-				if(v1.statnr==v.statnr){
-				    v1.plan = parseFloat(v.vzkd);
-				}
-			    });
-			    vzkdPlan += parseFloat(v.vzkd);
-			    }
-			    
-			});
-			$scope.vzkd.vzkdPlan = vzkdPlan;
-		    }
-		});
-    }
-    
-    $scope.getVzkdArray = function(){
-	return $http.post('../getVzKdAktual.php',{kdvon:111,kdbis:195})
-		.then(function (response) {
-		    $scope.vzkdArray = response.data.vzkdArray;
-		    console.log('getVzkdArray');
-		    console.log($scope.vzkdArray);
-		    var vzkdSum = 0;
-		    if($scope.vzkdArray!==null){
-			$scope.vzkdArray.forEach(function(v){
-			    if(v.statnr!='X'){
-				$scope.vzkdAktual[v.statnr] = parseFloat(v.vzkd);
-				vzkdSum += parseFloat(v.vzkd);
-			    }
-			});
-			$scope.vzkd.vzkdAktual = vzkdSum;
-		    }
-		});
-    }
-    
-    
-    $scope.vzkdAktual = {};
+    var vzkdAktual = {};
     
     $scope.vzkd = {
-	vzkdAktual:45000,
-	vzkdPlan:55000,
-	vzkdSchatzung:44000
+	vzkdAktual:0,
+	vzkdPlan:0,
+	vzkdSchatzung:0,
+	vzkdStat:{}
     };
     
     $scope.statArray = [
@@ -200,10 +56,59 @@ aplApp.controller('vzkdController', function ($scope, $http,$timeout,$window,$lo
 	}
     ];
     
+    // funkce ==================================================================
+    
+    $scope.getStatArray = function(){
+	return $http.post(
+		'../getStatNrPlan.php',
+		{kdvon:111,kdbis:195} // nevyuziva se, vybiram podle produktgruppe = 1
+		).then(function (response) {
+		    $scope.statNrArray = response.data.statArray;
+		    var vzkdPlan = 0;
+		    if($scope.statNrArray!==null){
+			$scope.statNrArray.forEach(function(v){
+			    if(v.statnr!='S0091'){
+				$scope.statArray.forEach(function(v1){
+				if(v1.statnr==v.statnr){
+				    v1.plan = parseFloat(v.vzkd);
+				}
+			    });
+			    vzkdPlan += parseFloat(v.vzkd);
+			    }
+			    
+			});
+			$scope.vzkd.vzkdPlan = vzkdPlan;
+		    }
+		});
+    }
+    
+    $scope.getVzkdArray = function(){
+	return $http.post('../getVzKdAktual.php',{kdvon:111,kdbis:195})
+		.then(function (response) {
+		    $scope.vzkdArray = response.data.vzkdArray;
+		    var vzkdSum = 0;
+		    if($scope.vzkdArray!==null){
+			$scope.vzkdArray.forEach(function(v){
+			    if(v.statnr!='X'){
+				vzkdAktual[v.statnr] = parseFloat(v.vzkd);
+				vzkdSum += parseFloat(v.vzkd);
+			    }
+			});
+			$scope.vzkd.vzkdAktual = vzkdSum;
+			$scope.vzkd.vzkdStat = vzkdAktual;
+		    }
+		});
+    }
+    
+    
+    
+    
     $scope.getStatArray();
     $scope.getVzkdArray();
     
     $interval($scope.getVzkdArray,15000);
+    $interval($scope.getStatArray,60000);
+    
     $interval(function(){
 	$scope.dt = new Date();
     },1000);
--- utils/gauges/js/directives.js
+++ utils/gauges/js/directives.js
@@ -23,159 +23,232 @@ gApp.directive('halfdonutView', function () {
 	    }
 	});
 
-function renderDonutView($scope,$elements,$attrs){
-    
-    console.log('renderDonutView');
-//    console.log($elements);
+function renderDonutView($scope, $elements, $attrs) {
+
     var width = $attrs.width;
     var height = $attrs.height;
     var svg = d3.select($elements[0]).append("svg");
     var data = $scope.data;
     var vzkdData = $scope.vzkd;
     var dt = $scope.dt;
-    //console.log(data);
+
+    // nastavit sirku svg elementu
     svg.attr(
 	    {
-		width:width,
-		height:height
+		width: width,
+		height: height
 	    });
 
+    // zjistim sirku vysku svg elementu v pixelech
     var svgWidth = parseInt(svg.style('width'));
     var svgHeight = parseInt(svg.style('height'));
 
+
     var graph = svg.append('g')
-	    .attr('transform','translate('+svgWidth/2+','+svgHeight+')');
+	    .attr('transform', 'translate(' + svgWidth / 2 + ',' + svgHeight + ')');
 
-    // sledovani zmen vzkd
-    $scope.$watch("vzkd",function(){
-	console.log('watch vzkd');
-	graph.select('text.vzkdHodnota')
-	    .text(function(d) {
-		return numeral(vzkdData.vzkdAktual).format('0,0');
-	    });	    
+    var colors = d3.scale.ordinal()
+	    .domain([0, 1, 2, 3, 4, 5])
+	    .range(['#FFC8FA', '#D2BEFF', '#F5B95F', '#FFFFB4', '#FFAAAA', '#00FF00']);
 
-    var rest = vzkdData.vzkdPlan-vzkdData.vzkdAktual;
-    if(rest<0){
-	// prekrocili jsme plan
-	fill = '#ff0';
-	rest = 0;
-    }
-    else{
-	fill = '#c00';
-    }
-    var pathVzKd = graph.selectAll('path.vzkd')
-	    .data(pieVzKd([vzkdData.vzkdAktual,rest]))
-//	    .transition()
-//	    .delay(200)
-	    .attr('fill',function(d,i){
-		var c = [fill,'#eee'];
-		return c[i];
-	    })
-	    .attr('d',arcVzKd);
-    },true);
-    
-    // sledovani zmen v rozdeleni planu na statisticke oblasti
-    $scope.$watch("data",function(){
-	console.log('watch data');
-	var path = graph.selectAll('path.stat')
-	    .data(pie(data))
-//	    .transition()
-//	    .delay(1000)
-	    .attr('d',arc);
-    },true);
-    
-    //sleduju zmenu datetime
-    $scope.$watch("dt",function(){
-	console.log('watch dt');
-	var timeFormat = d3.time.format('%d.%m.%Y %H:%M:%S');
-	graph.select('text.dt')
-	    .text(function(d) {
-		return timeFormat($scope.dt);
-	    });	    
-    },true);
-    
-    
-    //console.log(data);
-    var maxPlan = d3.max(data,function(d){return d.plan});
-    //console.log(maxPlan);
-   var colors = d3.scale.ordinal()
-    .domain([0,1,2,3,4,5])
-    .range(['#FFC8FA', '#D2BEFF' , '#F5B95F','#FFFFB4','#FFAAAA','#00FF00']);
-    
-    console.log(colors.domain());
-   //var colors = d3.scale.category10();
-    var colorsVzKd = [d3.rgb('#fcc'),d3.rgb('#fff')];
+    var colorsVzKd = [d3.rgb('#fcc'), d3.rgb('#fff')];
+
     //var radius = Math.min(svgWidth, svgHeight) / 2;
+
     var radius = svgWidth / 2;
+
+    // oblouk pro donut pro plan jednotlivych statnr
     var arc = d3.svg.arc()
 	    .outerRadius(radius)
-	    .innerRadius(3*radius/4);
-    
+	    .innerRadius(3 * radius / 4);
+
+    // oblouk pro donut pro skutecne vzkd jednotlivych statnr
+    var inPadding = 20;
+    var arcStatAktual = d3.svg.arc()
+	    .outerRadius(radius - inPadding)
+	    .innerRadius(3 * radius / 4 + inPadding);
+
+    // oblouk pro skutecne vzkd v sume
     var arcVzKd = d3.svg.arc()
-	    .outerRadius(3*radius/4-5)
+	    .outerRadius(3 * radius / 4 - 10)
 	    .innerRadius(50);
-    
+
+    // useky pro planovane vzkd pro jednotlive statnr
     var pie = d3.layout.pie()
-	    .startAngle(-Math.PI/2)
-	    .endAngle(Math.PI/2)
+	    .startAngle(-Math.PI / 2)
+	    .endAngle(Math.PI / 2)
 	    .padAngle(0.00)
-	    .value(function(d){return d.plan})
+	    .value(function (d) {
+		return d.plan
+	    })
 	    .sort(null);
-    
+
+    // useky pro sumu skutecnych vzkd
     var pieVzKd = d3.layout.pie()
-	    .startAngle(-Math.PI/2)
-	    .endAngle(Math.PI/2)
-	    .value(function(d){return d})
+	    .startAngle(-Math.PI / 2)
+	    .endAngle(Math.PI / 2)
+	    .value(function (d) {
+		return d
+	    })
 	    .sort(null);
-    
+
+
     var path = graph.selectAll('path .stat')
 	    .data(pie(data))
 	    .enter()
 	    .append('path')
-	    .attr('class','stat')
-	    .attr('d',arc)
-	    .attr('fill',function(d,i){
-		return d3.rgb(colors(i)).brighter(0.6);
-		//return colors(i);
+	    .attr('class', 'stat')
+	    .attr('d', arc)
+	    .attr('stroke', '#ccc')
+	    .attr('stroke-width', '3')
+
+	    .attr('fill', function (d, i) {
+		return '#EEEEEE';
+		//return d3.rgb(colors(i)).brighter(0.65);
 	    });
-	    
-    var pathVzKd = graph.selectAll('path .vzkd')
-	    .data(pieVzKd([vzkdData.vzkdAktual,vzkdData.vzkdPlan-vzkdData.vzkdAktual]))
+
+    dataStatAktual = pie(data).map(function (v) {
+	var scale = d3.scale.linear().domain([0, v.value]).range([v.startAngle, v.endAngle]);
+	return {
+	    startAngle: v.startAngle,
+	    endAngle: v.startAngle,
+	    value: 0,
+	    padAngle: 0, //v.padAngle,
+	    data: v.data
+	};
+    });
+    // upravit koncovy uhel podle aktualnich hodnot vzkd
+    var pathStat = graph.selectAll('path .stataktual')
+	    .data(dataStatAktual)
 	    .enter()
 	    .append('path')
-	    .attr('class','vzkd')
-	    .attr('d',arcVzKd)
-	    .attr('fill',function(d,i){
-		var c = ['#000','#ccc'];
+	    .attr('class', 'stataktual')
+	    .attr('d', arcStatAktual)
+	    .attr('fill', function (d, i) {
+		return colors(i);
+	    });
+
+    var pathVzKd = graph.selectAll('path .vzkd')
+	    .data(pieVzKd([vzkdData.vzkdAktual, vzkdData.vzkdPlan - vzkdData.vzkdAktual]))
+	    .enter()
+	    .append('path')
+	    .attr('class', 'vzkd')
+	    .attr('d', arcVzKd)
+	    .attr('fill', function (d, i) {
+		var c = ['#000', '#ccc'];
 		return c[i];
 	    });
 
     graph.append('text')
-	.attr({
-	    class:'vzkdHodnota',
-	    //fill: '#00ff00',
-	    x: 0,
-	    y: -(3*radius/4-5)/2,
-	    "text-anchor":'middle',
-	    stroke:'#fff',
-	    'stroke-width':'2px'
+	    .attr({
+		class: 'vzkdHodnota',
+		x: 0,
+		y: -(3 * radius / 4 - 5) / 2,
+		"text-anchor": 'middle',
+		stroke: '#fff',
+		'stroke-width': '2px'
 	    })
-	    .text(function(d) {
+	    .text(function (d) {
 		return vzkdData.vzkdAktual;
-	    });	    
+	    });
 	    
     graph.append('text')
-	.attr({
-	    class:'dt',
-	    //fill: '#00ff00',
-	    x: 0,
-	    y: -80,
-	    "text-anchor":'middle',
-	    stroke:'#fff',
-	    'stroke-width':'2px'
+	    .attr({
+		class: 'vzkdPlan',
+		x: 0,
+		y: -(3 * radius / 4 - 5) / 3,
+		"text-anchor": 'middle',
+		stroke: '#fff',
+		'stroke-width': '2px'
+	    })
+	    .text(function (d) {
+		return numeral(vzkdData.vzkdPlan).format('0,0');
 	    });
+
+    graph.append('text')
+	    .attr({
+		class: 'dt',
+		//fill: '#00ff00',
+		x: 0,
+		y: -80,
+		"text-anchor": 'middle',
+		stroke: '#fff',
+		'stroke-width': '2px'
+	    });
+
+
+    // updatovani pri zmenach dat ----------------------------------------------
+    // sledovani zmen vzkd
+    $scope.$watch("vzkd", function () {
+
+	graph.select('text.vzkdPlan')
+	    .text(function (d) {
+		//var procenta = parseFloat(vzkdData.vzkdPlan)!==0:numeral(parseFloat(vzkdData.vzkdAktual)/parseFloat(vzkdData.vzkdPlan)*100).format('0.00'):'';
+		return 'Plan: '+numeral(vzkdData.vzkdPlan).format('0,0');
+	    });
+	    
+	graph.select('text.vzkdHodnota')
+		.text(function (d) {
+		    return numeral(vzkdData.vzkdAktual).format('0,0');
+		});
+
+	var rest = vzkdData.vzkdPlan - vzkdData.vzkdAktual;
+	if (rest < 0) {
+	    // prekrocili jsme plan
+	    fill = '#ff0';
+	    rest = 0;
+	}
+	else {
+	    fill = '#c00';
+	}
+	graph.selectAll('path.vzkd')
+		.data(pieVzKd([vzkdData.vzkdAktual, rest]))
+		.attr('fill', function (d, i) {
+		    var c = [fill, '#eee'];
+		    return c[i];
+		})
+		.attr('d', arcVzKd);
+
+	dataStatAktual = pie(data).map(function (v) {
+	    var scale = d3.scale.linear().domain([0, v.value]).range([v.startAngle, v.endAngle]);
+	    var vzkdA = parseFloat(vzkdData.vzkdStat[v.data.statnr]);
+	    return {
+		startAngle: v.startAngle,
+		endAngle: scale(vzkdA),
+		value: vzkdA,
+		padAngle: 0, //v.padAngle,
+		data: v.data
+	    };
+	});
+	// upravit koncovy uhel podle aktualnich hodnot vzkd
+	var pathStat = graph.selectAll('path.stataktual')
+		.data(dataStatAktual)
+		.attr('d', arcStatAktual)
+		.attr('fill', function (d, i) {
+		    return colors(i);
+		});
+    }, true);
+
+    // sledovani zmen v rozdeleni planu na statisticke oblasti
+    $scope.$watch("data", function () {
+	var path = graph.selectAll('path.stat')
+		.data(pie(data))
+		.attr('d', arc);
+    }, true);
+
+    //sleduju zmenu datetime
+    $scope.$watch("dt", function () {
+	var timeFormat = d3.time.format('%d.%m.%Y %H:%M:%S');
+	graph.select('text.dt')
+		.text(function (d) {
+		    return timeFormat($scope.dt);
+		});
+    }, true);
 }
 
+
+
+// -----------------------------------------------------------------------------
 /**
  * 
  * @param {type} $scope
